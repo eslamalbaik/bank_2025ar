838561aa4661fbc1b3bc6312a6b0a2861b3b14f9
Eslam AlBaik <89574402+eslamalbaik@users.noreply.github.com>
2025-06-25 16:44:30 +0300

    Update button labels and links in Services component
    Changed the label and link for the 'القروض' button and updated the label for the second button in the third row from 'سحب غزة' to 'حذف الأجهزة' to better reflect their intended actions.

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -12,11 +12,11 @@ const Services = () => {
     </div>
     <div className="flex justify-center items-center gap-4 w-full mt-6">
     <Button text_btn="تفعيل iBURAQ" link_btn="subs"/>  
-     <Button text_btn="القروض" link_btn="bankloans"/>
+     <Button text_btn="القروض" link_btn="subs"/>
     </div>
     <div className="flex justify-center items-center gap-4 w-full mt-6">
      <Button text_btn="تجديد البطاقه" link_btn="subs"/>
-    <Button text_btn="سحب غزة" link_btn="subs"/>
+    <Button text_btn="حذف الأجهزة" link_btn="subs"/>
     </div>
     <div className='mt-6'>
     <Button text_btn="تفعيل التطبيق بدون انترنت" link_btn="subs"/>
